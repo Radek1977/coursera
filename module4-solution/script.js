7b436c4d1a20b2c8135c818e2909a3413f7c1ed5
Radek <Radoslaw@lapko.net>
2017-02-23 22:53:10 +0100

    steps 1-10

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,6 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
+(function(){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -41,8 +42,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (i = 0; i < 10; i++) {
 
+console.log(i);
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -57,9 +59,10 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+ // if (/* fill in condition here */) {
     // byeSpeaker.xxxx
-  } else {
+  //} else {
     // helloSpeaker.xxxx
-  }
+  //}
 }
+})();
