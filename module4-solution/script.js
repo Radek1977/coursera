9c9995b25cae32768c58bbc3cd81648945d030aa
Radek <Radoslaw@lapko.net>
2017-02-28 20:33:11 +0100

    endversion

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -43,8 +43,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // 'speak' method.
 // See Lecture 50, part 1
 for (i = 0; i < 10; i++) {
-
-console.log(i);
+  var firstLetter = names[i].charAt(0).toLowerCase();
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -59,10 +58,13 @@ console.log(i);
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
- // if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  //} else {
-    // helloSpeaker.xxxx
-  //}
-}
+   if (firstLetter == 'j') 
+    {
+       byeSpeaker.speak(names[i]);
+    } 
+    else 
+    {
+      helloSpeaker.speak(names[i]);
+    }
+  }
 })();
